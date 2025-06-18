82507344b237cf0ef05601fdb47f79c6b406b956
Kristijan Vuković <vukovickristijan24@gmail.com>
2025-06-18 03:45:22 +0200

    Fixed responsiveness, added content to 'content cards'.

---
+++ javascript/variables.js
@@ -0,0 +1,6 @@
+const moviesCard = document.getElementById("moviesCard");
+const tvShowsCard = document.getElementById("tvShowsCard");
+const animeCard = document.getElementById("animeCard");
+const gamesCard = document.getElementById("gamesCard");
+
+const contentCards = document.getElementById("contentCards");
