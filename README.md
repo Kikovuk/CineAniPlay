cbe2e8505a560a053a960587a44492855b5e87e3
Kristijan Vuković <vukovickristijan24@gmail.com>
2025-07-06 01:29:21 +0200

    Transferring to Node.js and React.

--- README.md
+++
@@ -1,2 +0,0 @@
-# CineAniPlay
-CineAniPlay website
--- javascript/variables.js
+++
@@ -1,6 +0,0 @@
-const moviesCard = document.getElementById("moviesCard");
-const tvShowsCard = document.getElementById("tvShowsCard");
-const animeCard = document.getElementById("animeCard");
-const gamesCard = document.getElementById("gamesCard");
-
-const contentCards = document.getElementById("contentCards");
